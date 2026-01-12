8b7e602e054faf471d867924799d977e2139289e
Fadhiel <fmaulana@student.telkomuniversity.ac.id>
2026-01-12 11:44:21 +0700

    refactor: log

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`dailybakes Backend running on: http://localhost:${port}/api`);
+  console.log(`dailybakes Backend Service running on: http://localhost:${port}/api`);
 }
 bootstrap();
